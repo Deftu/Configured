ae3a5190a0d4b0cede0e600d5ca93fd394c35b35
Deftu <deftudev@gmail.com>
2022-06-01 13:18:04 +0200

    Fix preprocessor statements.

--- src/main/kotlin/xyz/unifycraft/configured/options/OptionSerializer.kt
+++ src/main/kotlin/xyz/unifycraft/configured/options/OptionSerializer.kt
@@ -22,7 +22,7 @@ class OptionSerializer {
     val initialized: Boolean
         get() = this::config.isInitialized && this::collector.isInitialized
     var dirty = false
-    //#if MC<=11605
+    //#if MC<=11701
     private val jsonParser = JsonParser()
     //#endif
 
@@ -74,7 +74,7 @@ class OptionSerializer {
     }
 
     fun parseJson(str: String) =
-        //#if MC<=11605
+        //#if MC<=11701
         jsonParser.parse(str)
         //#else
         //$$ JsonParser.parseString(str)
